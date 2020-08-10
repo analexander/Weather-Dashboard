2907d175b94edc94ee552c645246ec3c67541eb2
Angel Alexander <angale22@gmail.com>
2020-08-09 20:54:50 -0500

    added functions for lat/lon and uv index

--- script.js
+++ script.js
@@ -18,18 +18,42 @@ function searchWeather(city) {
     var cityTemp = $("<h2>").text(response.main.temp);
     var cityHumid = $("<h2>").text(response.main.humidity);
     var cityWindSpeed = $("<h2>").text(response.wind.speed);
-    var cityUVI = $("<h2>").text(response.uvi);
 
     $("#weather-output").empty();
-    $("#weather-output").append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUVI);
+    $("#weather-output").append(cityName, cityTemp, cityHumid, cityWindSpeed);
 
     });
 
+}
+
+function getLatLon(location){
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=2ad6f6ff3e813152c0dc8257bf34ef21",
+        method: "GET"
+      }).then(function(data){
+        console.log(data)
+
+        //Use UVIndex function here while you still have access to data,
+        //else it is erased for being out of scope
+        getUVIndex(data.coord.lat, data.coord.lon)
+    })
+
+    
+}
+
+function getUVIndex(lat, lon){
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=2ad6f6ff3e813152c0dc8257bf34ef21",
+        method: "GET"
+    }).then(function(data){
+        console.log(data.value);
+    });
 }
 
 $("#search-btn").on("click", function(event) {
     event.preventDefault();
     // This line of code will grab the input from the textbox
     var cityNameInput = $("#search-input").val().trim();
+    getLatLon(cityNameInput)
     searchWeather(cityNameInput)
 });
